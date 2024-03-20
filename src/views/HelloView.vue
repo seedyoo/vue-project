22c0708781cd71b7e124059f93bbaf087dba7a43
seed-yoo <lukdown10@naver.com>
2024-03-20 16:19:05 +0900

    2024/03/20

---
+++ src/views/HelloView.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <h1>Hello Vue.js</h1>
+
+</template>
+
+<script>
+export default {
+    name: "HelloView",
+    data(){
+        return{
+            
+        }
+    }
+}
+
+</script>
+
+<style></style>
